ac2e00ab1e35833d77af3487989dcb1dd5568967
jason <jason_xxx7@163.com>
2020-01-16 18:23:22 +0800

    4.在pages/index.vue配置轮播图静态页面

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,56 @@
 <template>
-    <div>
-        首页1111
+    <div class="container">
+    <!-- 通过Element-ui的走马灯组件el-carousel完成轮播图布局 -->
+    <!-- 此处仍是使用静态页面，未使用后台 -->
+    <el-carousel
+    :interval="5000"
+    arrow="always">
+    <el-carousel-item
+    v-for="(item, index) in banners"
+    
+    :key="index">
+    <!-- 给轮播图相应样式避免缩小时缩小，以背景图形式 -->
+    <div class="banner-image"
+    :style="`background:url(${item.url}) center center no-repeat;
+    background-size:contain contain`">
+    </div>
+    </el-carousel-item>
+    </el-carousel>  
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            //轮播图数据（网络图片）
+            banners:[
+                {
+                    url:"http://157.122.54.189:9095/assets/images/th03.jfif"
+                },
+                {
+                    url: "http://157.122.54.189:9095/assets/images/th04.jfif", 
+                }
+            ]
+        }
+    }
 
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
