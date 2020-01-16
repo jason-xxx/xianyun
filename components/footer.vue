12f9be4dc13475af53e598d8a8fbd96c24b25ee7
jason <jason_xxx7@163.com>
2020-01-16 17:30:15 +0800

    生成首页，并关联头部组件与页尾组件
    1.生成header.vue
    2.生成footer.vue

---
+++ components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="foot">
+这是尾部
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,60 @@
 <template>
   <div>
+    <!-- 头部组件 -->
+    <Header/>
     <!-- 相当于 router-view -->
     <nuxt />
+
+    <!--页尾组件-->
+      <Footer/>
   </div>
 </template>
 
-<style>
+<script>
+// 导入头部组件
+import Header from "@/components/header"
+//导入页尾组件
+import Footer from '@/components/footer'
 
+export default {
+  // 注册组件
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="less">
+//样式初始化开始-------------------------------
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin:0; 
+  padding:0;
+}
+ul, li, ol{
+  list-style:none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+}
+a:hover{
+
+}
+em,i{
+  font-style: normal;
+}
+// 样式初始化结束----------------------------------------------
 </style>
