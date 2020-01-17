5c4f5ee3c69ecc4f2f6c8789134b6a374b62e9cf
jason <jason_xxx7@163.com>
2020-01-17 15:57:01 +0800

    6.2生成登录表单组件并在登录页面调用
    在components/user中新建loginForm.vue表单组件

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -19,22 +19,26 @@
                 </el-row>
 
                 <!-- 登录功能组件 -->
-                <!-- <LoginForm v-if="currentTab == 0"/> -->
+                <LoginForm v-if="currentTab == 0"/>
 
                 <!-- 注册功能组件 -->
-                <!-- <RegisterForm v-if="currentTab == 1"/> -->
+                <RegisterForm v-if="currentTab == 1"/>
             </div>
         </el-row>
     </div>
 </template>
 
 <script>
+import LoginForm from "@/components/user/loginForm";
 export default {
     data(){
         return {
             currentTab: 0
         }
     },
+      components: {
+    LoginForm
+  },
     methods: {
         handleChangeTab(index){
             this.currentTab = index;
